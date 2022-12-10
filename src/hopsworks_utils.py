4317e1809ca90a9635d7a168e22defe05051519a
Chris Munch <cmunch1@yahoo.com>
2022-12-10 08:50:11 -0600

    create dict before saving

--- src/hopsworks_utils.py
+++ src/hopsworks_utils.py
@@ -4,7 +4,7 @@ import json
 def convert_feature_names(df: pd.DataFrame)-> pd.DataFrame:
 
     '''
-    Converts hopsworks.ai feature names back to original feature names that are saved in JSON file
+    Converts hopsworks.ai lower-case feature names back to original mixed-case feature names that are saved in JSON file
     '''
 
     # hopsworks converts all feature names to lower-case, while the original feature names use mixed-case
@@ -14,11 +14,8 @@ def convert_feature_names(df: pd.DataFrame)-> pd.DataFrame:
 
     # read in list of original feature names
     with open('feature_names.json', 'rb') as fp:
-        original_f_names = json.load(fp)
+        feature_mapper = json.load(fp)
         
-    # create a dictionary
-    feature_mapper = {hopsworks_f_names[i]: original_f_names[i] for i in range(len(hopsworks_f_names))}
-
     df = df.rename(columns=feature_mapper)
 
     return df
@@ -26,14 +23,19 @@ def convert_feature_names(df: pd.DataFrame)-> pd.DataFrame:
 def save_feature_names(df: pd.DataFrame)-> pd.DataFrame:
 
     '''
-    Saves original feature name mixed-case formatting to JSON file
+    Saves dictionary of {lower case feature names : original mixed-case feature names} to JSON file
     '''
     # hopsworks "sanitizes" feature names by converting to all lowercase
     # this function saves the original so that they can be re-mapped later
     # for code re-usability
     
-    feature_names = df.columns.tolist()
+    original_f_names = df.columns.tolist()
+    hopsworks_f_names = [x.lower() for x in original_f_names]
+
+    # create a dictionary
+    feature_mapper = {hopsworks_f_names[i]: original_f_names[i] for i in range(len(hopsworks_f_names))}
+
     with open("feature_names.json", "w") as fp:
-        json.dump(feature_names, fp)
+        json.dump(feature_mapper, fp)
         
     return "File Saved."
