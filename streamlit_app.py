c35feae2b765a5bcb8c8e88d9e7b5545a012737e
Chris Munch <cmunch1@yahoo.com>
2022-12-11 20:02:11 -0600

    testing

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,10 +42,44 @@ def get_model(project, model_name, evaluation_metric, sort_metrics_by):
     model = mr.get_best_model(model_name,
                                 evaluation_metric,
                                 sort_metrics_by)
-    model_dir = model.download()
-    model = joblib.load(model_dir + "/model.pkl")
+    
+    model = joblib.load("model.pkl")
 
     return model
+
+nba_team_names = {
+    1610612737: "Atlanta Hawks",
+    1610612738: "Boston Celtics",
+    1610612739: "Cleveland Cavaliers",
+    1610612740: "New Orleans Pelicans",
+    1610612741: "Chicago Bulls",
+    1610612742: "Dallas Mavericks",
+    1610612743: "Denver Nuggets",
+    1610612744: "Golden State Warriors",
+    1610612745: "Houston Rockets",
+    1610612746: "Indiana Pacers",
+    1610612747: "LA Clippers",
+    1610612748: "Los Angeles Lakers",
+    1610612749: "Memphis Grizzlies",
+    1610612750: "Miami Heat",
+    1610612751: "Milwaukee Bucks",
+    1610612752: "Minnesota Timberwolves",
+    1610612753: "Brooklyn Nets",
+    1610612754: "New York Knicks",
+    1610612755: "Orlando Magic",
+    1610612756: "Philadelphia 76ers",
+    1610612757: "Phoenix Suns",
+    1610612758: "Portland Trail Blazers",
+    1610612759: "Sacramento Kings",
+    1610612760: "San Antonio Spurs",
+    1610612761: "Oklahoma City Thunder",
+    1610612762: "Toronto Raptors",
+    1610612763: "Utah Jazz",
+    1610612764: "Washington Wizards",
+    1610612765: "Detroit Pistons",
+    1610612766: "Charlotte Hornets",
+}
+
 
 
 st.title('NBA Prediction Project')
@@ -79,6 +113,7 @@ progress_bar.progress(40)
 
 # prepare data for prediction
 
+# convert feature names back to mixed case
 df_todays_matches = convert_feature_names(df_todays_matches)
 
 df_todays_matches = fix_datatypes(df_todays_matches)
@@ -86,10 +121,14 @@ df_todays_matches = fix_datatypes(df_todays_matches)
 drop_columns = ['TARGET', 'GAME_DATE_EST', 'GAME_ID', ] 
 df_todays_matches = df_todays_matches.drop(drop_columns, axis=1)
 
+df_todays_matches['MATCHUP'] = df_todays_matches['VISITOR_TEAM_ID'].map(nba_team_names) + " @ " + df_todays_matches['HOME_TEAM_ID'].map(nba_team_names)
+
 use_columns = remove_non_rolling(df_todays_matches)
 
 X = df_todays_matches[use_columns]
+X = X.drop('MATCHUP', axis=1)
 
+st.write(df_todays_matches['MATCHUP'])
 
 print(use_columns)
 
@@ -101,21 +140,24 @@ progress_bar.progress(60)
 st.write(36 * "-")
 fancy_header(f"🗺 Predicting Win Probabilities...")
 
+st.write("Predicting win probabilities for today's games...")
 
+#model = get_model(project=project,
+#                  model_name="xgboost",
+#                  evaluation_metric="AUC",
+#                  sort_metrics_by="max")
 
-model = get_model(project=project,
-                  model_name="xgboost",
-                  evaluation_metric="AUC",
-                  sort_metrics_by="max")
+model = joblib.load("model.pkl")
 
 preds = model.predict(X_dmatrix)
 
+df_todays_matches['HOME_TEAM_WIN_PROBABILITY'] = preds
 
-print(preds)
+st.dataframe(df_todays_matches[['MATCHUP', 'HOME_TEAM_WIN_PROBABILITY']])
 
 
-df = pd.DataFrame(data=preds, columns=[f"Predictions for Today's Games"])
 
-st.sidebar.write(df)
+
+
 progress_bar.progress(100)
 st.button("Re-run")
