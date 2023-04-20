b95844edabb5fd427e0369594af7e1a305f030f2
Chris Munch <cmunch1@yahoo.com>
2023-04-20 09:09:10 -0500

    correct prediction logic

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -247,10 +247,10 @@ df_current_season['HOME_TEAM_WIN_PROBABILITY'] = preds
 df_current_season = df_current_season.rename(columns={'TARGET': 'HOME_WINS'})
 
 # add column to show if prediction was correct
-df_current_season['CORRECT_PREDICTION'] = ((df_current_season['HOME_TEAM_WIN_PROBABILITY'] > 0.5) and (df_current_season['HOME_WINS'] == 1)) or ((df_current_season['HOME_TEAM_WIN_PROBABILITY'] < 0.5) and (df_current_season['HOME_WINS'] == 0))
+#df_current_season['CORRECT_PREDICTION'] = ((df_current_season['HOME_TEAM_WIN_PROBABILITY'] > 0.5) and (df_current_season['HOME_WINS'] == 1)) or ((df_current_season['HOME_TEAM_WIN_PROBABILITY'] < 0.5) and (df_current_season['HOME_WINS'] == 0))
+df_current_season['CORRECT_PREDICTION'] = int(df_current_season['HOME_TEAM_WIN_PROBABILITY']) == df_current_season['HOME_WINS'] 
 
-
-st.dataframe(df_current_season[['GAME_DATE_EST','MATCHUP', 'HOME_TEAM_WIN_PROBABILITY', 'HOME_WINS?', 'CORRECT_PREDICTION?']])
+st.dataframe(df_current_season[['GAME_DATE_EST','MATCHUP', 'HOME_TEAM_WIN_PROBABILITY', 'HOME_WINS', 'CORRECT_PREDICTION']])
 
 # Show accuracy
 st.write("Accuracy: " + str(df_current_season['CORRECT_PREDICTION'].sum() / df_current_season.shape[0]))
